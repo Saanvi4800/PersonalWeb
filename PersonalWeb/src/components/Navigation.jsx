2047c7c7113022d99e9b86a77fd9f74700962a5f
Saanvi4800 <Saanvi4800@gmail.com>
2026-07-06 20:18:33 -0500

    im just doing stuff atp lowk

--- PersonalWeb/src/components/Navigation.jsx
+++ PersonalWeb/src/components/Navigation.jsx
@@ -1,60 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import './Navigation.css';
-import frog from "/src/assets/frog2.jpeg";
-import Icon from '@hackclub/icons'
-
-
+import frog from "../assets/frog2.jpeg";
+import face from "../assets/facec.png";
+import Icon from '@hackclub/icons';
 
 function Navigation() {
+  const [currentImage, setCurrentImage] = useState(frog);
+
+  const toggleImage = () => {
+    setCurrentImage(prev => (prev === frog ? face : frog));
+  };
+
   return (
-  <div className="parent-nav">
-    <nav className="navbar">
-      <ul>
-        <figure>
-         <div className="circle-container">
-           <img id= "frog" src={frog} onClick={toggleImage} alt="user" className="circle-img" />
-         </div>
-         <figcaption>*click me!*</figcaption>
-        </figure>
-         <div className="contact" style={{color: "beige"}}>
-                <a href="https://github.com/Saanvi4800" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph="github" size={25} />
-                </a>
-        
-                <a href="https://www.instagram.com/saanvitrip24/" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph="instagram" size={25} />
-                </a>
-        
-                <a href="mailto:Saanvi4800@gmail.com">
-                  <Icon glyph="email" size={25} />
-                </a>
-        
-                <a href="https://www.linkedin.com/in/saanvi-tripathi-88b977381/" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph="linkedin" size={25} />
-                </a>
-        
-                <a href="https://hackclub.enterprise.slack.com/team/U09984Y84BD" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph="slack" size={25} />
-                </a>
-              </div>
-        <li><Link to="/">home</Link></li>
-        <li><Link to="/projects">projects</Link></li>
-        <li><Link to="/diy">diys</Link></li>
-       
-      </ul>   
-    </nav>
-  </div>
+    <div className="parent-nav">
+      <nav className="navbar">
+        <ul>
+          <figure>
+            <div className="circle-container">
+              <img
+                id="frog"
+                src={currentImage}
+                onClick={toggleImage}
+                alt="user"
+                className="circle-img"
+              />
+            </div>
+            <figcaption>*click me!*</figcaption>
+          </figure>
+
+          <div className="contact" style={{ color: "beige" }}>
+            <a href="https://github.com/Saanvi4800" target="_blank" rel="noopener noreferrer">
+              <Icon glyph="github" size={25} />
+            </a>
+            <a href="https://www.instagram.com/saanvitrip24/" target="_blank" rel="noopener noreferrer">
+              <Icon glyph="instagram" size={25} />
+            </a>
+            <a href="mailto:Saanvi4800@gmail.com">
+              <Icon glyph="email" size={25} />
+            </a>
+            <a href="https://www.linkedin.com/in/saanvi-tripathi-88b977381/" target="_blank" rel="noopener noreferrer">
+              <Icon glyph="linkedin" size={25} />
+            </a>
+            <a href="https://hackclub.enterprise.slack.com/team/U09984Y84BD" target="_blank" rel="noopener noreferrer">
+              <Icon glyph="slack" size={25} />
+            </a>
+          </div>
+
+          <li><Link to="/">home</Link></li>
+          <li><Link to="/projects">projects</Link></li>
+          <li><Link to="/diy">diys</Link></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
 export default Navigation;
-
-function toggleImage() {
-  const image = document.getElementById("frog");
-  if (image.src.includes("frog2.jpeg")) {
-    image.src = "/src/assets/facec.png";
-  } else {
-    image.src = "/src/assets/frog2.jpeg";
-  }
-}
